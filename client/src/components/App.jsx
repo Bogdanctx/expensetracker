e0954a6a39434750b1733a915ca3ca8f463a645f
bgd <trifan.bogdan09@yahoo.com>
2025-04-14 12:00:22 +0300

    Added transaction component (#8)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,24 +5,24 @@ import ExpandedAccountView from './ExpandedAccountView';
 import Sidebar from './Sidebar';
 import NewAccount from './NewAccount';
 import './App.css';
+import Transaction from './Transaction';
 
 function App() {
     const [accounts, setAccounts] = useState([]);
+    const [transactions, setTransactions] = useState([]);
     const [selectedAccount, setSelectedAccount] = useState(null);
     const [displayNewAccountCard, setDisplayNewAccountCard] = useState(false);
 
     useEffect(() => {
-        const fetchAccounts = async () => {
-          try {
-            const response = await axios.get("http://localhost:8080/api/accounts");
-            setAccounts(response.data);
-            
-          } catch (err) {
-
+        const fetchData = async (setter, link) => {
+            try {
+                const response = await axios.get(link);
+                setter(response.data);
+            } catch (err) {}
         }
-        };
-    
-        fetchAccounts();
+        
+        fetchData(setAccounts, "http://localhost:8080/api/accounts");
+        fetchData(setTransactions, "http://localhost:8080/api/transactions");
       }, []);
     
 
@@ -30,14 +30,25 @@ function App() {
         <div style={{ display: "flex" }}>
             <Sidebar setDisplayNewAccountCard={setDisplayNewAccountCard} />
             <div id="content">
-                <div style={{opacity: selectedAccount ? "0.4" : "1"}}>
-                    {displayNewAccountCard && (
-                        <NewAccount setDisplayNewAccountCard={setDisplayNewAccountCard} />
-                    )}
-                    {accounts.map((account) => (
-                        <Account key = {account.id} account={account} setSelectedAccount={setSelectedAccount} />
-                    ))}
+                <div id="first-half">
+                    <div style={{opacity: selectedAccount ? "0.4" : "1"}}>
+                        {displayNewAccountCard && (
+                            <NewAccount setDisplayNewAccountCard={setDisplayNewAccountCard} />
+                        )}
+                        {accounts.map((account) => (
+                            <Account key = {account.id} account={account} setSelectedAccount={setSelectedAccount} />
+                        ))}
+                    </div>
                 </div>
+
+                <div id="second-half">
+                    <div id="upper-box">
+                        {transactions.map((transaction) => (
+                            <Transaction key = {transaction.id} transaction={transaction} />
+                        ))}
+                    </div>
+                </div>
+                
                     {selectedAccount && (
                         <ExpandedAccountView account={selectedAccount} setSelectedAccount={setSelectedAccount} />
                     )}
